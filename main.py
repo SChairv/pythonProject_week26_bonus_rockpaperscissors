468ec76e9955e76b0eae9c266fec7c96de0067f6
SChairv <110042823+SChairv@users.noreply.github.com>
2023-01-28 10:26:40 -0500

    updated to make this a real game with a computer choice to play against.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #Create a program that simulates rock, paper, scissors!
+from random import randint
+
 again = ""
 
 def Rochambeau(x,y):
+    print("The computer has chosen", y, ".")
     if x == y: print("It's a tie!"),
     elif (x=="rock" and y == "paper") or (x=="paper" and y=="rock") : print ("Paper beats rock!"),
     elif (x=="rock" and y =="scissors") or (x=="scissors" and y=="rock") : print("Rock beats scissors!"),
@@ -12,8 +15,11 @@ def Rochambeau(x,y):
 while again!="no" :
     x=(input("Please enter rock, paper, or scissors "))
     x=(x).lower()
-    y=(input("Please enter rock, paper, or scissors "))
-    y=(y).lower()
+    y=randint(1,3)
+    if y==1: y="paper"
+    elif y==2: y="rock"
+    elif y==3: y="scissors"
+
     Rochambeau(x,y)
     again = input("Would you like to play again?")
     again = again.lower()
